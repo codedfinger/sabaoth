b69b04dd86b11caf869258e385c2448ebaadacc1
codedfingers <alexyikeh@gmail.com>
2023-05-19 12:34:24 +0100

    chore: update infra details on yaml file

--- .circleci/deploy_frontend.sh
+++ .circleci/deploy_frontend.sh
@@ -2,7 +2,7 @@
 
 # Define the target EC2 instance and subnet details
 INSTANCE_IP="$EC2_INSTANCE_IP_WEB"  # Replace with the IP address of your EC2 instance
-SSH_KEY="$PATH_TO_SSH_KEY"  # Replace with the path to your SSH key file
+SSH_KEY="$SSH_PRIVATE_KEY"  # Replace with the path to your SSH key file
 SUBNET_ID="$SUBNET_ID"  # Replace with the ID of the subnet where your EC2 instance resides
 
 # Copy the built files to the EC2 instance
