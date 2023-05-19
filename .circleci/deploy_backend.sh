82729d30c06e69680c4c8e5e08c6d68a853b9eba
codedfingers <alexyikeh@gmail.com>
2023-05-19 15:08:27 +0100

    chore: update infra details on yaml file

--- .circleci/deploy_backend.sh
+++ .circleci/deploy_backend.sh
@@ -2,11 +2,11 @@
 
 # Define the target EC2 instance and subnet details
 INSTANCE_IP="$EC2_INSTANCE_IP_APP"  # Replace with the IP address of your EC2 instance
-SSH_KEY="$SSH_PRIVATE_KEY"  # Replace with the path to your SSH key file
+SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY"  # Replace with the path to your SSH key file
 SUBNET_ID="$SUBNET_ID"  # Replace with the ID of the subnet where your EC2 instance resides
 
 # Copy the built files to the EC2 instance
-scp -o StrictHostKeyChecking=no -i "$SSH_KEY" -r backend/* ubuntu@"$INSTANCE_IP":/home/ubuntu/backend
+scp -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" -r backend/* ubuntu@"$INSTANCE_IP":/home/ubuntu/backend
 
 # Connect to the EC2 instance and restart the Express app
-ssh -o StrictHostKeyChecking=no -i "$SSH_KEY" -v ubuntu@"$INSTANCE_IP" "cd /home/ubuntu/backend && npm install && pm2 start index.js"
+ssh -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" -v ubuntu@"$INSTANCE_IP" "cd /home/ubuntu/backend && npm install && pm2 start index.js"
--- .circleci/deploy_frontend.sh
+++ .circleci/deploy_frontend.sh
@@ -2,11 +2,11 @@
 
 # Define the target EC2 instance and subnet details
 INSTANCE_IP="$EC2_INSTANCE_IP_WEB"  # Replace with the IP address of your EC2 instance
-SSH_KEY="$SSH_PRIVATE_KEY"  # Replace with the path to your SSH key file
+SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY"  # Replace with the path to your SSH key file
 SUBNET_ID="$SUBNET_ID"  # Replace with the ID of the subnet where your EC2 instance resides
 
 # Copy the built files to the EC2 instance
-ssh -o StrictHostKeyChecking=no -i "$SSH_KEY" -v ubuntu@"$INSTANCE_IP" "sudo mkdir -p /var/www/html"
+ssh -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" -v ubuntu@"$INSTANCE_IP" "sudo mkdir -p /var/www/html"
 
 # Connect to the EC2 instance and restart the Express app
-scp -o StrictHostKeyChecking=no -i "$SSH_KEY" -r frontend/build/* ubuntu@"$INSTANCE_IP":/var/www/html/
+scp -o StrictHostKeyChecking=no -i "$SSH_PRIVATE_KEY" -r frontend/build/* ubuntu@"$INSTANCE_IP":/var/www/html/
