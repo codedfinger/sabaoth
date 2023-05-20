9105d8043502400b50e86a5bb0eafd88a2d3d0fe
codedfingers <alexyikeh@gmail.com>
2023-05-20 05:31:00 +0100

    chore: update infra details on yaml file

--- .circleci/deploy_frontend.sh
+++ .circleci/deploy_frontend.sh
@@ -6,7 +6,7 @@ SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY"  # Replace with the path to your SSH key file
 SUBNET_ID="$SUBNET_ID"  # Replace with the ID of the subnet where your EC2 instance resides
 
 # Copy the built files to the EC2 instance
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -v ubuntu@"$INSTANCE_IP" "sudo mkdir -p /var/www/html"
+ssh -o StrictHostKeyChecking=no -i -v ubuntu@"$INSTANCE_IP" "sudo mkdir -p /var/www/html"
 
 # Connect to the EC2 instance and restart the Express app
-scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -r frontend/build/* ubuntu@"$INSTANCE_IP":/var/www/html/
+scp -o StrictHostKeyChecking=no -i -r frontend/build/* ubuntu@"$INSTANCE_IP":/var/www/html/
