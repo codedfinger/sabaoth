b22bae3261d24ce0aaf24946a3fc4a8d435f03e7
codedfingers <alexyikeh@gmail.com>
2023-05-20 05:19:25 +0100

    chore: update infra details on yaml file

--- .circleci/deploy_backend.sh
+++ .circleci/deploy_backend.sh
@@ -5,12 +5,8 @@ INSTANCE_IP="$EC2_INSTANCE_IP_APP"  # Replace with the IP address of your EC2 in
 SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY"  # Replace with the path to your SSH key file
 SUBNET_ID="$SUBNET_ID"  # Replace with the ID of the subnet where your EC2 instance resides
 
-echo "$SSH_PRIVATE_KEY" > sabaoth.pem
-
-chmod 400 sabaoth.pem
-
 # Copy the built files to the EC2 instance
-scp -o StrictHostKeyChecking=no -i sabaoth.pem -r backend/* ubuntu@"$INSTANCE_IP":/home/ubuntu/backend
+scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -r backend/* ubuntu@"$INSTANCE_IP":/home/ubuntu/backend
 
 # Connect to the EC2 instance and restart the Express app
-ssh -o StrictHostKeyChecking=no -i sabaoth.pem -v ubuntu@"$INSTANCE_IP" "cd /home/ubuntu/backend && npm install && pm2 start index.js"
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -v ubuntu@"$INSTANCE_IP" "cd /home/ubuntu/backend && npm install && pm2 start index.js"
