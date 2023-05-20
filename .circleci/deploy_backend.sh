e9715c6b51808d782543e20111cc2ad1f632a78d
codedfingers <alexyikeh@gmail.com>
2023-05-20 05:44:37 +0100

    chore: update infra details on yaml file

--- .circleci/deploy_backend.sh
+++ .circleci/deploy_backend.sh
@@ -6,7 +6,7 @@ SSH_PRIVATE_KEY="$SSH_PRIVATE_KEY"  # Replace with the path to your SSH key file
 SUBNET_ID="$SUBNET_ID"  # Replace with the ID of the subnet where your EC2 instance resides
 
 # Copy the built files to the EC2 instance
-scp -o StrictHostKeyChecking=no -i -r backend/* ubuntu@"$INSTANCE_IP":/home/ubuntu/backend
+scp -o StrictHostKeyChecking=no -i -r backend/* ubuntu@52.23.164.165:/home/ubuntu/backend
 
 # Connect to the EC2 instance and restart the Express app
-ssh -o StrictHostKeyChecking=no -i -v ubuntu@"$INSTANCE_IP" "cd /home/ubuntu/backend && npm install && pm2 start index.js"
+ssh -o StrictHostKeyChecking=no -i -v ubuntu@52.23.164.165 "cd /home/ubuntu/backend && npm install && pm2 start index.js"
